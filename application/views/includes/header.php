10890a6c581c0fdd10bcdca8abfb97d2e3f1cc82
remy <remy@remysharp.com>
2010-06-13 17:54:01 +0100

    favicon

--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -2,14 +2,11 @@
 <html lang="en" id="leftlogic">
 <head>
   <meta charset="utf-8">
-  <title>Left Logic</title>
-  <link rel="icon" type="image/png" href="/favicon.png">
-  
-  <!--[if IE]><script>/*@cc_on'abbr article aside audio canvas details figcaption figure footer header hgroup mark menu meter nav output progress section summary time video'.replace(/\w+/g,function(n){document.createElement(n)})@*/</script><![endif]-->
-
-  <link rel="stylesheet" href="/assets/css/leftlogic.css">
-  <!--[if IE 6]><link rel="stylesheet" href="/assets/css/ie6.css"><![endif]-->
-  
+  <title>Left Logic - Specialist Web Development</title>
+  <link rel="icon" type="image/png" href="/image/favicon.png" />
+  <!--[if lte IE 8]><script>/*@cc_on'abbr article aside audio canvas details figcaption figure footer header hgroup mark menu meter nav output progress section summary time video'.replace(/\w+/g,function(n){document.createElement(n)})@*/</script><![endif]-->
+  <link rel="stylesheet" href="/assets/css/leftlogic.css" />
+  <!--[if IE 6]><link rel="stylesheet" href="/assets/css/ie6.css" /><![endif]-->
 </head>
 <body<?php echo $motif ? ' class="motif"' : ''; ?>>
 <div id="wrapper">
