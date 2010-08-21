6216c1f3d11cbdf499ff376c8771867e9e63c67e
remy <remy@remysharp.com>
2010-08-21 19:50:44 +0100

    quick fix

--- application/views/missing.php
+++ application/views/missing.php
@@ -4,7 +4,7 @@
     <p>Nope, looking around and couldn't find what you were looking for.</p>
     <p>If it wasn't important, how about you play in this box I've left you:</p>
     <div id="play"></div>
-    <?php if (count($files)) : shuffle($files); ?>
+    <?php if (count($files) && is_array($files)) : shuffle($files); ?>
     <div id="others">
       <h2>Other 404 images people have left behind:</h2>
       <?php for ($i = 0; $i < 8 && @$files[$i]; $i++) : ?><img src="/images/generated/<?=$files[$i]?>" width="64" /><?php endfor ?>
