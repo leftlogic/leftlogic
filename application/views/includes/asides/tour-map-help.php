ec40f66730c74ba1d308d2abbba916b0794d87c6
remy <remy@remysharp.com>
2011-02-03 11:20:42 +0000

    typo

--- application/views/includes/asides/tour-map-help.php
+++ application/views/includes/asides/tour-map-help.php
@@ -1,7 +1,7 @@
 <section><div>
   <h2>Map Tips</h2>
   <ul>
-    <li>Double click pin to remove</li>
+    <li>Double click pin to delete it</li>
     <li>You can drag pins around</li>
     <li>More than one pin is allowed</li>
     <li><p>Left Logic logo shows where others have put pins in the map</p></li>
