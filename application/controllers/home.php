3e5004c2b107e4f600b2e231f042e2843db126b7
remy <remy@remysharp.com>
2011-03-17 19:35:14 +0000

    radiohead on homepage

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -10,9 +10,10 @@ class Home extends MY_Controller {
 	function index() {
 	  
 	  $data['recent'] = array(
+	    array('title' => 'Radiohead animation', 'client' => 'DevStars/Radiohead', 'img' => '/images/portfolio/small/radiohead.gif', 'url' => 'http://radiohead.com'),
 	    array('title' => 'Google Holiday Card', 'client' => 'Google', 'img' => '/images/portfolio/small/google-maps-xmas.png', 'url' => 'http://googleblog.blogspot.com/2010/12/send-message-of-holiday-cheer-with.html'),
-      array('title' => 'A Tale of Two Cities', 'client' => 'Nestoria', 'img' => '/images/portfolio/small/nestoria.png', 'url' => 'http://ataleoftwocities.nestoria.co.uk/'),
       array('title' => 'iPlayer performance review', 'client' => 'BBC', 'img' => '/images/portfolio/small/iplayer.png'),
+      array('title' => 'A Tale of Two Cities', 'client' => 'Nestoria', 'img' => '/images/portfolio/small/nestoria.png', 'url' => 'http://ataleoftwocities.nestoria.co.uk/'),
       array('title' => 'Lucky Voice.home', 'client' => 'Torchbox', 'img' => '/images/portfolio/small/luckyvoice.png', 'url' => 'http://home.luckyvoice.com/')
 	  );
 	  
