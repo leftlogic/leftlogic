4a2c889cda9b99542a2050a2d3b2ed2ddceb6f00
Remy Sharp <remy@remysharp.com>
2012-01-26 17:00:16 +0000

    why node

--- application/controllers/training.php
+++ application/controllers/training.php
@@ -11,6 +11,11 @@ class Training extends MY_Controller {
 	  $data['asides'] = 'latest-talks';
 		$this->view('training', $data);
 	}
+
+	function whynode() {
+		$data['asides'] = 'latest-talks';
+		$this->view('training/whynode', $data);	
+	}
 	
 	function consultancy() {
 	  
