26093e6c272027249418e6e20a6f1982538c37e7
Remy Sharp <remy@remysharp.com>
2013-11-25 22:33:09 +0000

    tidy up training

--- application/views/training.php
+++ application/views/training.php
@@ -5,20 +5,22 @@
     <p>Left Logic has packaged some of our expertise. We now offer specific training workshops to in-house teams wishing to brush up on their development skills, from latest HTML5 technology, mobile development &amp; debugging to jQuery for Designers.</p>
     <p>Left Logic also offers a tailored consultancy service. <a href="/contact">(Contact us for more information)</a></p>
 
-    <section class="focus">
-      <h2>Upcoming workshops</h2>
+     <section class="focus">
+      <h2>Workshops</h2>
       <ul>
-        <li><a href="node1day">Node: knowledge smash &amp; grab in a day</a> / £395+vat / July 19</li>
-        <li><a href="j4d">jQuery for Designers &amp; beginners</a> / £395+vat / July 18</li>
+        <li><a href="#debug">Debug, and know thy tools</a></li>
+        <li><a href="#node1day">Node: knowledge smash &amp; grab in a day</a></li>
+        <li><a href="#mobile">Mobile Web Apps</a></li>
+        <li><a href="#html5">Using HTML5 and JavaScript to build Web Apps
+</a></li>
       </ul>
-      <p>Student discounts of 50% off are also available on both workshops.</p>
     </section>
 
     <h2>Comments from workshop delegates</h2>
     <blockquote cite="George Hedges, Sussex Downs College">I thought content level was pitched just right. Very relaxed and friendly atmosphere. A great day.</blockquote>
     <blockquote cite="Andy Birchwood">Remy is a rare type &mdash; a developer who understand designers and has a passion for how things look. He presented some complex but elegant jQuery concepts in a straightforward way.</blockquote>
 
-  </section><!-- /sub-section-alt -->
+  </section>
 
 
   <section class="workshop" id="node1day">
@@ -51,6 +53,27 @@
         </li>
       </ul>
 
+      <h3 id="node_2_day_option">2 day option available</h3>
+
+      <h4 id="day_one">Day 1: An introduction to Node</h3>
+
+      <ul>
+      <li>The basics: Async development, modules, requires, exports and scope</li>
+      <li>Debugging techniques</li>
+      <li>Building web servers: by hand, with Connect, with Express.js</li>
+      <!-- <li>Working with document databases (mongodb &amp; mongoose)</li> -->
+      <li>Repo control, deploying using npm and release tips</li>
+      </ul>
+
+      <h4 id="day_two">Day 2: Using HTML5 tech with Node</h3>
+
+      <ul>
+        <li>Going real time: WebSockets and Socket.IO</li>
+        <li>Building WebSockets from scratch and using Socket.IO</li>
+        <li>Using WebSockets in the browser, with and without support</li>
+        <li>Server sent events and long polling: building the server and clients</li>
+      </ul>
+
       <h3 id="what_do_you_need_for_this_workshop">What you need for this workshop?</h3>
 
       <ul>
@@ -60,7 +83,7 @@
         <li>A browser with latest technology baked in, like <a href="http://www.google.com/chrome">Google Chrome</a> for instance</li>
       </ul>
 
-      <h3>When, where, how much</h3>
+      <!-- <h3>When, where, how much</h3>
       <ul>
         <li>Cost: <span class="cost">£395 + <abbr>VAT</abbr> per person</span></li>
         <li>When: 19 July 2013<br></li>
@@ -69,26 +92,68 @@
 
       <h3 id="ticket_also_includes">Ticket includes:</h3>
         <p>All tickets include lunch and refreshments in the morning and afternoon. For special dietary requirements, please let us know.</p>
-
+ -->
       <div id="node-book-now" class="calltoaction">
         <h3>Book now!</h3>
         <!-- <p>When booking, include your company details and the names and email address of each delegate as we will raise an invoice and email the delegates directly with directions and instructions for the day.</p> -->
-        <ul>
+<!--         <ul>
           <li><a class="button" href="https://leftlogic.stagehq.com/events/2287">Book now</a></li>
         </ul>
-        <p>We can also <a href="/contact?subject=Node%20workshop%0A%0APlease book the following individual/s on to the Node workshop:%0A%0AName: %0AEmail:%0A%0ARaise the invoice for: <YOUR COMPANY>">raise an invoice</a> for your company if you'd prefer to pay via invoice.</p>
-        <!-- <p>Get in touch and arrange a company workshop</p> -->
-        <!-- <ul><li><a class="button" href="/contact?subject=Node%20%26%20HTML5%20for%20a%20real-time%20web">Enquire now</a></li></ul> -->
+        <p>We can also <a href="/contact?subject=Node%20workshop%0A%0APlease book the following individual/s on to the Node workshop:%0A%0AName: %0AEmail:%0A%0ARaise the invoice for: <YOUR COMPANY>">raise an invoice</a> for your company if you'd prefer to pay via invoice.</p> -->
+        <p>Get in touch and arrange a company workshop</p>
+        <ul><li><a class="button" href="/contact?subject=Node%20%26%20HTML5%20for%20a%20real-time%20web">Enquire now</a></li></ul>
       </div>
     </section>
   </section>
 
+  <section class="workshop" id="debug">
+    <img src="/images/debug-workshop.jpg" />
+    <section>
+
+      <h2>Debug, and know thy tools</h2>
+
+      <p>Sure, you've moved past <code>alert</code> debugging and discovered console.log, but did you know you can replay XHR requests instead of having to repeat your steps to make the request? Or that you can proxy your local server through a browser configuration so your mobile phone can see it? This workshop is dedicated to learning the native developer tools baked in to the browser to make our life a little more bearable.
+      </p>
+
+      <p>We will primarily look at the debugging features of Chrome DevTools and those concepts that apply to other browsers. We'll also look at remote mobile debugging using native tools and also look at the alternative tools where native support isn't available.</p>
+
+      <p>The workshop will mostly be about the foundation of debugging techniques, which apply way beyond just a single browser's debugger tool, but will raise your expectation of all web debugging environments.
+      </p>
+
+      <h3 id="debug_what_you_learn">What you'll learn</h3>
+      <ul>
+        <li><em><strong>Workflow</strong></em>: Using the tools you know to simply your daily workflow</li>
+        <li><em><strong>Mobile</strong></em>: Using the tools you know to debug tiny machines</li>
+        <li><em><strong>Console</strong></em>: Power features beyond console.log</li>
+        <li><em><strong>Network</strong></em>: What's slow, why and the detail behind a request</li>
+        <li><em><strong>Memory</strong></em>: How to identify and diagnose memory leaks in the wild</li>
+        <li><em><strong>Performance</strong></em>: How to read and investigate rendering issues</li>
+      </ul>
+
+      <h3 id="what_do_you_need_for_this_workshop">What you need for this workshop?</h3>
+
+      <ul>
+        <li>A text editor (Sublime Text, TextMate, Coda, Textpad++, etc)</li>
+        <li><a href="http://www.google.com/chrome">Google Chrome</a> and <a href="http://www.google.co.uk/intl/en/chrome/browser/canary.html">Google Canary</a></li>
+        <li><a href="http://nodejs.org">Node installed</a> (make sure it's a recent version)</li>
+      </ul>
+
+      <div id="node-book-now" class="calltoaction">
+        <h3>Book now!</h3>
+        <p>Get in touch and arrange a company workshop</p>
+        <ul><li><a class="button" href="/contact?subject=Debugging">Enquire now</a></li></ul>
+      </div>
+    </section>
+  </section>
+
+<?php
+// getting rid of this workshop entirely
+/*
 
   <section class="workshop" id="j4d">
     <img src="/images/html5-workshop.jpg" title="Photo by Stefan Nitzsche" />
     <section>
       <h2>jQuery for Designers</h2>
-<?php /*      <span class="cost">£349 + <abbr>VAT</abbr> pp</span> */ ?>
       <p>You've heard of jQuery, you know it's supposed to do beautiful things, but you don't do JavaScript, and you don't know where all the pieces go to create <em>your</em> beautiful thing. This workshop will ease you in to adding jQuery to your projects, cover what jQuery has to offer and how you can make use of it.</p>
 
       <p>You'll be learning jQuery from a practical point of view: what you can use out of the box to quickly get effects and interaction live in to your wireframes, prototypes and web sites. The aim <em>isn't</em> to teach you JavaScript, but to teach you what you need to know to be able to leverage jQuery to do your bidding.</p>
@@ -111,23 +176,23 @@
       <h3>Who is this workshop for?</h3>
       <p>This workshop is for front-end visual designers and developers who are familiar with HTML and CSS. If you have flirted with jQuery or JavaScript in the past, this will be an advantage, but is not a necessity.</p>
 
-      <h3>When, where, how much</h3>
+<!--       <h3>When, where, how much</h3>
       <ul>
         <li>Cost: <span class="cost">£395 + <abbr>VAT</abbr> per person</span></li>
         <li>When: 18 July 2013<br></li>
         <li>Where: The Lighthouse, Brighton (<a href="http://maps.google.co.uk/maps/place?q=the+lighthouse+brighton&cid=1375416526688458247">map</a>)</li>
       </ul>
-
+ -->
       <div id="j4d-book-now" class="calltoaction">
         <h3>Book now!</h3>
-        <ul>
+<!--         <ul>
           <li><a class="button" href="https://leftlogic.stagehq.com/events/2286">Book now</a></li>
         </ul>
         <p>We can also <a href="/contact?subject=jQuery%20for%20Designers%20workshop%0A%0APlease book the following individual/s on to the jQuery for Designers workshop:%0A%0AName: %0AEmail:%0A%0ARaise the invoice for: <YOUR COMPANY>">raise an invoice</a> for your company if you'd prefer to pay via invoice.</p>
-      </div>
+ -->      </div>
     </section>
   </section>
-
+*/?>
 
   <section class="workshop" id="mobile">
     <img src="/images/mobile-workshop.jpg" />
@@ -175,14 +240,14 @@
 
       <div id="mobile-book-now" class="calltoaction">
         <h3>Book today</h3>
-        <p>When booking, include your company details and the names and email address of each delegate as we will raise an invoice and email the delegates directly with directions and instructions for the day.</p>
-        <ul>
+        <!-- <p>When booking, include your company details and the names and email address of each delegate as we will raise an invoice and email the delegates directly with directions and instructions for the day.</p> -->
+<!--         <ul>
           <li><a class="button" href="/contact?subject=Mobile%20Workshop%0A%0APlease book the following individual/s on to the Mobile workshop:%0A%0AName: %0AEmail:%0A%0ARaise the invoice for: <YOUR COMPANY>">Book now</a></li>
-        </ul>
-        <!-- <p>Get in touch and arrange a company workshop</p>
+        </ul> -->
+        <p>Get in touch and arrange a company workshop</p>
         <ul>
           <li><a class="button" href="/contact?subject=Mobile%20Workshop">Enquire now</a></li>
-        </ul> -->
+        </ul>
       </div>
     </section>
   </section>
@@ -191,7 +256,7 @@
     <img src="/images/html5-workshop2.jpg" />
     <section>
       <h2>Using HTML5 and JavaScript to build Web Apps</h2>
-<?php /*      <span class="cost">£349 + <abbr>VAT</abbr> pp</span>*/?>
+
       <p>HTML5 has gained a lot of attention over the last 12 months. With browsers increasingly supporting the features of the vast JavaScript APIs both in and around the official HTML5 spec, it's making the job of creating awesome applications purely using these web technologies very easy indeed.</p>
       <p>This full day workshop will introduce you to HTML5 with a brief backstory, before diving into the APIs one by one. As well as going through code and showing practical demonstrations, where possible, we'll also talk about the alternatives for old browsers that don't support "awesome" out of the box.</p>
 
