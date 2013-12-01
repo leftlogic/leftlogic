049993bdcf92e14247eb6732aa8c41bb0fcc66fb
Remy Sharp <remy@remysharp.com>
2013-12-01 23:24:25 +0100

    add missing aside

---
+++ application/views/includes/asides/jsbin.php
@@ -0,0 +1,5 @@
+<section>
+  <h2>Latest Project</h2>
+  <a href="http://jsbin.com"><img src="/images/aside-jsbin-plain.png" /></a>
+  <a href="http://jsbin.com" class="block-link">JS Bin</a>
+</section>
