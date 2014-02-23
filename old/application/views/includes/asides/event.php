fd172e7591c05dd01c5c9b2dee4c94bb001bfdb5
David Gauld <dcgauld@gmail.com>
2014-02-23 15:52:36 +0000

    Removed old website as reference can be found in master branch

--- old/application/views/includes/asides/event.php
+++
@@ -1,5 +0,0 @@
-<section>
-  <h2>Upcoming Events</h2>
-  <a href="http://full-frontal.org"><img src="/images/event-fullfrontal-2010.png" /></a>
-  <a href="http://full-frontal.org" class="block-link">Find out more</a>
-</section>
