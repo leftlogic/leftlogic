514e980e7114a9274075f0c0804323c269cf8b9a
David Gauld <dcgauld@gmail.com>
2014-03-19 18:54:03 +0000

    Added contact form POST route and 'thank you' page

--- routes.js
+++ routes.js
@@ -1,8 +1,24 @@
 'use strict';
 
+var SendGrid = require('sendgrid').SendGrid;
+var sendgrid = new SendGrid(
+  process.env.SENDGRID_USERNAME,
+  process.env.SENDGRID_PASSWORD
+);
+
 module.exports = function (app) {
   app.post('/contact', function (req, res) {
-    // handle sending email out
-    res.redirect('/contact-thanks');
+    return sendgrid.send({
+      from: req.body.email,
+      to: 'dcgauld@gmail.com', // Testing at the mo.
+      subject: 'Contact via leftlogic.com from ' + req.body.name,
+      text: req.body.message
+    }, function (err, message) {
+      if (err) {
+        console.error(err);
+        return console.log(message);
+      }
+      return res.redirect('/contact/thanks');
+    });
   });
 };
