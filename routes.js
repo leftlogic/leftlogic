a39b38c7437bb764d6e09295a50d3e4c17a04ea4
Remy Sharp <remy@remysharp.com>
2014-04-04 16:19:11 +0100

    404 and copy edits

--- routes.js
+++ routes.js
@@ -1,6 +1,9 @@
 'use strict';
 
 var sendgrid = require('sendgrid')(process.env.SENDGRID_USERNAME, process.env.SENDGRID_PASSWORD);
+var fs = require('fs');
+console.log(__dirname);
+var fourohfour = null;
 
 module.exports = function (app) {
   app.post('/contact', function (req, res) {
@@ -16,5 +19,36 @@ module.exports = function (app) {
       }
       return res.redirect('/contact/thanks');
     });
+  });
+
+  // 404
+  app.use(function (req, res, next) {
+    if (!fourohfour) {
+      try {
+        fourohfour = fs.readFileSync(__dirname + '/www/404.html');
+      } catch (e) {
+
+      }
+    }
+
+    res.status(404);
+
+    // respond with html page
+    if (req.accepts('html')) {
+      res.header('content-type', 'text/html');
+      res.send(fourohfour);
+      return;
+    }
+
+
+    // respond with json
+    if (req.accepts('json')) {
+      res.send({ error: 'Not found' });
+      return;
+    }
+
+    // default to plain-text. send()
+    res.type('txt').send('Not found');
+
   });
 };
