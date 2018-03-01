4ea725e20bcbd39a869b24d51a50073b62ed4a89
Remy Sharp <remy@remysharp.com>
2018-03-01 10:01:50 +0000

    fix: add react

--- next.config.js
+++ next.config.js
@@ -9,6 +9,7 @@ module.exports = {
       '/training/mobile': { page: '/training/mobile' },
       '/training/node': { page: '/training/node' },
       '/training/html': { page: '/training/html' },
+      '/training/react': { page: '/training/react' },
       '/training/whynode': { page: '/training/whynode' },
       '/404': { page: '/404' },
     };
